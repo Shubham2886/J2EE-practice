2018445512e80076aae576b9722e6182f088548a
Shubham Sharma <101943611+Shubham2886@users.noreply.github.com>
2023-01-18 21:21:17 +0530

    Add files via upload

--- SpringbootTest/src/main/java/com/met/iit/SpringbootTest/com/met/rest/RESTController.java
+++ SpringbootTest/src/main/java/com/met/iit/SpringbootTest/com/met/rest/RESTController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.met.iit.SpringbootTest.com.met.emp.Employee;
 import com.met.iit.SpringbootTest.com.met.services.EmployeeServices;
+@CrossOrigin
 @RestController
 @RequestMapping("/emp")
 public class RESTController {
